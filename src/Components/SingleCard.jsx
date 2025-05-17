9f894e4357392de8a9d33aca318a13bb0839c3bd
hiteshsachdev2002-Developer <hiteshsachdev2611@gmail.com>
2025-05-17 22:07:33 +0530

    youtube cards build up
    Youtube Card Build Up

--- src/Components/SingleCard.jsx
+++ src/Components/SingleCard.jsx
@@ -1,37 +1,26 @@
 
-function SingleCard()
+function SingleCard({title, thumbnail, channelName, channelImage, views, uploadedTime, duration}) 
 {
- const video = {
-    title: "Understanding JavaScript Closures",
-    thumbnail: "https://i.ytimg.com/vi/vKJpN5FAeF4/hq720.jpg",
-    channel: {
-      name: "CodeAcademy",
-      profileImage: "https://yt3.ggpht.com/Vzv2OJNdnqMMuBwBp4RMI-4Mp5V3ouabPv0yO69EuxHxtazM12v7DFEOOna9ZEbcS3go-0hb=s176-c-k-c0x00ffffff-no-rj-mo"
-    },
-    views: "1.2M",
-    uploadedTime: "2 days ago",
-    duration: "15:32"
-  };
 
   return (
     <div className="youtube-card">
       <div className="thumbnail-container">
-        <img src={video.thumbnail} alt="Thumbnail" className="thumbnail" />
-        <span className="duration">{video.duration}</span>
+        <img src={thumbnail} alt="Thumbnail" className="thumbnail" />
+        <span className="duration">{duration}</span>
       </div>
 
       <div className="video-info">
         <img
-          src={video.channel.profileImage}
+          src={channelImage}
           alt="Channel"
           className="channel-img"
         />
 
         <div className="video-details">
-          <h4 className="video-title">{video.title}</h4>
-          <p className="channel-name">{video.channel.name}</p>
+          <h4 className="video-title">{title}</h4>
+          <p className="channel-name">{channelName}</p>
           <p className="video-stats">
-            {video.views} views • {video.uploadedTime}
+            {views} views • {uploadedTime}
           </p>
         </div>
       </div>
@@ -39,4 +28,4 @@ function SingleCard()
   );
 }
 
-export default SingleCard;
+export default SingleCard
--- src/Components/YoutubeCards.jsx
+++ src/Components/YoutubeCards.jsx
@@ -1,9 +1,47 @@
+import { useState,useEffect } from "react";
+import axios from "axios";
 import SingleCard from "./SingleCard";
 function YoutubeCard() {
+  
+    const[cardData, setCardData] = useState([]);
+
+    async function getYoutubeData() {
+       
+        const youtube = await axios.get("https://66d7839637b1cadd8051ba0d.mockapi.io/nn/api/youtube");
+        setCardData(youtube.data);
+        console.log(youtube.data);
+    }
+
+    useEffect(() => {
+        getYoutubeData();
+    },[]);
+
+     
   return (
 
     <>
-        <SingleCard/>
+     <div className="youtube-card-content" >
+      {
+       
+        cardData.map((data) => {
+            return (
+                
+                <SingleCard
+                    title={data.title}
+                    thumbnail={data.thumbnail}
+                    channelName={data.channel?.name || "Unknown Channel"}
+                    channelImage={data.channel?.profileImage || ""}
+                    views={data.views}
+                    uploadedTime={data.uploadedTime}
+                    duration={data.duration}
+                />
+              
+            );
+        })
+       
+
+      }
+       </div>
     </>
     
   );
